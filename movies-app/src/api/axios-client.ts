eb69d3410684db34f20cada836c83e1633a09e61
RaikoGAL <raikogal@gmail.com>
2026-04-20 11:31:23 +0300

    chore: remove unused TMDB_TOKEN warning

--- movies-app/src/api/axios-client.ts
+++ movies-app/src/api/axios-client.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
-const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 export const axiosClient = axios.create({
   baseURL: TMDB_BASE_URL,
